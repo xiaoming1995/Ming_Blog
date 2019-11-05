40b54f0f0bd47403f6257d821e0b2c4a046069b5
LiuMing <460667926@qq.com>
2019-11-05 11:00:37 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 
-
 class UsersController extends Controller
 {
     public function create()
@@ -26,7 +25,15 @@ class UsersController extends Controller
     		'password' => 'required|confirmed|min:6'
     	]);
 
-    	return;
+    	$user = User::create([
+    		'name' 		=> $request->name,
+    		'email'		=> $request->email,
+    		'password' 	=> bcrypt($request->password),
+    	]);
+
+    	session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+    	return redirect()->route('users.show',[$user]);
+
     }
 
 }
