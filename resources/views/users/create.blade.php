a4302de764d73e34d0664721460265912bba0f4e
LiuMing <460667926@qq.com>
2019-11-05 08:23:58 +0000

    中文语言包及错误提示信息

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,6 +8,7 @@
       <h5>注册</h5>
     </div>
     <div class="card-body">
+    	@include('shared._errors')
       <form method="POST" action="{{ route('users.store') }}">
       	{{ csrf_field() }}
           <div class="form-group">
@@ -29,7 +30,6 @@
             <label for="password_confirmation">确认密码：</label>
             <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
           </div>
-
           <button type="submit" class="btn btn-primary">注册</button>
       </form>
     </div>
