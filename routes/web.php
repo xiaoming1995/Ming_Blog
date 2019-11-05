b29f74e8e4322a6b59cd51a2821bcc279746c31d
LiuMing <460667926@qq.com>
2019-11-05 21:15:30 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 //users
 Route::get('signup','UsersController@create')->name('signup'); //注册
 Route::resource('users','UsersController');
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
