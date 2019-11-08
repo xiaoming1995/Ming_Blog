a4ecc84e9a6c523bf09b85e98c2e31076a766e7f
LiuMing <460667926@qq.com>
2019-11-08 11:54:40 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user  = User::find(1);
         $user->name = 'Liuming';
         $user->email = '460667926@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,7 +5,9 @@
 <div class="offset-md-2 col-md-8">
   <h2 class="mb-4 text-center">所有用户</h2>
   <div class="list-group list-group-flush">
-    @include('users._user')
+    @foreach ($users as $user)
+      @include('users._user')
+    @endforeach
   </div>
 
   <div class="mt-3 index-paginate">
